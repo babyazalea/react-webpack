7459352f16cabaf21ff5612a5be4d5e69632288a
babyazalea <usun12@hotmail.com>
2021-01-19 19:26:11 +0900

    made basic folder structure & install react, webpack cli & webpack configuration

---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import PizzaImage from "../components/PizzaImage/Pizzaimage";
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>users on board of this course!</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
